26a47d9c75b5e62c7193004f2c8584231082dc5b
Elton <32944003+EltZ36@users.noreply.github.com>
2020-08-06 16:24:37 -0700

    Testing
    Commented out the input as I'm going to change it. Plan to solve the determinant and make it able to be infinite.

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 def solve():
  pass
 def start():
- while True:
-  try:
-    Question1 = int(input("How many rows?"))
-    Question2 = int(input("How many columns?"))
-    break
+  '''
+  while True:
+    try:
+      Question1 = int(input("How many rows?"))
+      Question2 = int(input("How many columns?"))
   except ValueError:
     print("Not an int. please try again")
     start()
     continue
+  else:
+    break
   return Question1
   return Question2
-print("Welcome to the determinant solver!")
+  '''
+  row = 3
+  column = 3
+  for i in range(0,row):
+    a = 1
+    b = 1
+    c = 1
+    d = 1
+    e = 1
+    f = 1
+    g = 1
+    h = 1
+    i = 1
+    A = [[a,b,c], [d,e,f], [g,h,i]]
+    if len(A) == column:
+      break
+  return A 
 start()
