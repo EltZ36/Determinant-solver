2216003b9efd48caeff19e719b8986ee25b2bb09
Elton <32944003+EltZ36@users.noreply.github.com>
2020-08-06 19:32:15 -0700

    Update main.py
    Added the matrix1() function and addition function. The addition function adds matrixes together and does not seem to be efficient for user input. I found this out online but I feel like there is a better way to do this. Matrix1() will be deleted soon and replaced with other operations. Start() now has user input in our to select the options and is the same as the question for the Temperature converter.

--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 import numpy as np
+
+def matrix1():
+  global A
+  rows = int(input("How many rows?"))
+  columns = int(input("How many columns?"))
+  print("Fill in the matrix below")
+  Array = input().split()
+  #list() creates an empty list and map runs the int function with the arguments of the input of rows and columns. the list cannot go over the inputed amount of rows or columns. Ex. I put 2 rows in the input and 2 columns in the input as well. When I fill in the matrix, I can't put in 3 rows and 1 column. 
+  Fill_in = list(map(int, Array))
+  A = np.array(Fill_in).reshape(rows, columns)
+  return A
+
+def addition():
+  rows = int(input("How many rows?"))
+  columns = int(input("How many columns?"))
+  print("Fill in the matrix below")
+  Array = input().split()
+  Fill_in = list(map(int, Array))
+  A = np.array(Fill_in).reshape(rows, columns)
+  rows2 = int(input("How many rows?"))
+  columns2 = int(input("How many columns?"))
+  print("Fill in the matrix below")
+  Array = input().split()
+  Fill_in = list(map(int, Array))
+  B = np.array(Fill_in).reshape(rows2, columns2)
+  Add = A + B
+  print(Add) 
+  
 def start():
-  '''
-  while True:
-    try:
-      Question1 = int(input("How many rows?"))
-      Question2 = int(input("How many columns?"))
-  except ValueError:
-    print("Not an int. please try again")
-    start()
-    continue
+  Question = int(input("Which option do you want to do? \n Addition(1) Multiplication (2) Division (3) Find Determinant(4)"))
+  print(Question)
+  if Question == 1:
+    addition()
+  elif Question == 2:
+    pass
+  elif Question == 3:
+    pass
+  elif Question == 4:
+    pass
   else:
-    break
-  return Question1
-  return Question2
-  '''
-  row = 3
-  for i in range(0,row):
-    x = 1
-    y = 1
-    z = 1
-    c1 = 0
-    x2 = 1
-    y2 = 1
-    z2 = 1
-    c2 = 0
-    x3 = 1
-    y3 = 1
-    z3 = 1
-    c3 = 0
-    matrix = np.array([[x,y,z,c1], [x2,y2,z2,c2], [x3,y3,z3,c3]])
-  return matrix 
+    print("Please put in the number next to the option you want.")
+    start()
 start()
+
 
